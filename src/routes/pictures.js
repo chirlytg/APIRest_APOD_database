b5c5c944f76e09bb0ef7b7a1ba9015ab132ccf5f
ShirleyG <shirley.galvis@udea.edu.co>
2022-05-29 17:03:53 -0500

    Revert "Functional get and push"
    This reverts commit aacb2e9771c33524a8e80105dbd2da9f0bb9cc2a.

--- src/routes/pictures.js
+++ src/routes/pictures.js
@@ -4,51 +4,32 @@ const numberOfPictures = 1;
 const router = Router();
 
 const fetch = require('node-fetch');
-let pictures = [];
 
 router.get('/', async (_req, res) => {
     //const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&count=${numberOfPictures}`)
     //const users = await response.json();
     //console.log(users);
     //res.json(users);
-	let picturess = [];
-
+	let pictures = []
 	try {
 		const { data } = await axios.get(
 			//`https://jsonplaceholder.typicode.com/user`
 			`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&count=${numberOfPictures}`
 		)
-		//const { data } = await fetch(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&count=${numberOfPictures}`)
 		for (const picture of data) {
 			const { explanation, hdurl, title, url } = picture
 			if(!explanation || !hdurl || !title || !url) continue
-			const id = picturess.length + 1;
-			picturess.push({
+			pictures.push({
 				explanation: explanation,
 				hdurl: hdurl,
 				title: title,
 				url: url,
-				id: id,
 			})
 		}
 	} catch (error) {
 		console.log("Error getting the pictures from the NASA API\n")
 	}
-	res.json(picturess);
-	//return pictures;
-});
-
-router.post('/', (req, res)=>{
-    const {explanation, hdurl, title, url} = req.body;
-    if(explanation && hdurl && title && url){ 
-		const id = pictures.length + 1;
-        const newpicture = {...req.body, id}; 
-        pictures.push(newpicture);
-        res.json(pictures);
-    }else{
-        res.json({error:'there was an error'});
-        //res.send('wrong request')
-    }
+	res.json(pictures);
 });
 
 module.exports = router;
